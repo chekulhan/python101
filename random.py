1f2d6bfb5513522ec6e38e4bbdd6eb847e738a45
chekulhan <46518731+chekulhan@users.noreply.github.com>
2023-01-24 18:17:34 +0100

    Create random.py

---
+++ random.py
@@ -0,0 +1,11 @@
+import random
+
+v1=random.random()
+v2 = random.randint(0,100)
+
+print(int(v1*100))
+print(v2)
+
+nombres= ['Arturo','Julio','Dani','Aurora','Roberto','Martina']
+nom=random.choice(nombres)
+print('El nombre elegido es:',nom)
