c88766e93f28606061e53b48967ecb4153ab06c6
chekulhan <46518731+chekulhan@users.noreply.github.com>
2023-01-31 12:09:25 +0100

    cantidad

--- flujo.respuestas.py
+++ flujo.respuestas.py
@@ -4,3 +4,13 @@ if color.lower() == "red":
   print("Lo mio es rojo también")
 else:
   print(f"Mi color favorito es rojo, no {color}")
+
+  
+  
+ qty = int(input("¿Cúal es la cantidad?"))
+
+if qty <0 or qty > 10:
+  print("Error: Introducir una cantidad >= 1 y <= 10")
+else:
+  print("Acceptado")
+
