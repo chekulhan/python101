74936b1f9a8a2f703e84373fbf009dfa792e7bd2
chekulhan <46518731+chekulhan@users.noreply.github.com>
2023-02-02 14:07:15 +0100

    Add files via upload

---
+++ modules/calculos.py
@@ -0,0 +1,18 @@
+##Para usar este modulo:
+#from modules.calculos import sumar, restar 
+
+## ejemplo:
+#print(sumar(4,5))
+#print(restar(4,5))
+
+## para ver las funciones en el módulo
+#from modules.calculos import * 
+#print(dir())
+
+# en versiones anteriores, hacia falta un archivo __init__.py en el directorio modules
+
+def sumar(a,b):
+    return a+b
+
+def restar(a,b):
+    return a-b
