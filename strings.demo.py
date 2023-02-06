68e3a06c18b8397e00641a7c483cb27ccafff79f
chekulhan <46518731+chekulhan@users.noreply.github.com>
2023-02-06 10:18:52 +0100

    Update strings.demo.py

--- strings.demo.py
+++ strings.demo.py
@@ -31,3 +31,26 @@ if BUSCAR in texto:
     print("Encontrado")
 else:
     print("No encontrado")
+
+    
+    
+# reto respuesta - Amazon sistema de registro
+
+# Palabras para excluir de la contraseña. Hay mejoras formas de hacer eso, con una lista, por ejemplo....
+EXCLUDE1 = "password"
+EXCLUDE2 = "contraseña"
+EXCLUDE3 = "123456"
+
+nombre = input("Cúal es tu nombre?")
+apellido = input("Cúal es tu apellido?")
+usuario = input("Introducir un nombre de usuario (mínimo 5 carácteres):")
+password = input("Introducir la contraseña (mínimo 6 carácteres, sin palabras típicas):")
+
+if len(usuario) < 5:
+    print("Por favor, vuelve a introducir un usuario.")
+elif len(password) < 6:
+    print("Por favor, vuelve a introducir una contraseña.")
+elif EXCLUDE1 in password.lower() or EXCLUDE2 in password.lower() or EXCLUDE3 in password:
+    print(f"Por favor, vuelve a introducir una contraseña sin palabras claves como {password}.")
+else:
+    print(f"Bienvenidos a Amazon {nombre.capitalize()}, {apellido.capitalize()}.")
