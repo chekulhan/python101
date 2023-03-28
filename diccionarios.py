c484292ab5e19112f875ab6c2128327a16e262f1
chekulhan <46518731+chekulhan@users.noreply.github.com>
2023-03-28 20:22:47 +0200

    JSON

--- diccionarios.py
+++ diccionarios.py
@@ -21,6 +21,20 @@ print("REP" in acciones.keys())
 acciones.update({"SAN": 8.71})
 print(acciones)
 
+# DEMO Diccionario a JSON 
+
+acciones = {"MSFT": 91.5, "REP": 7.91, "BBVA": 6.9, "OHL": 45.6}
+
+import json
+
+a = json.dumps(acciones)
+print(type(a))
+print(a)
+
+b = json.loads(a)
+print(type(b))
+print(b)
+
 
 # ACTIVIDAD
 acciones = {"MSFT": 91.5, "REP": 7.91, "BBVA": 6.9}
