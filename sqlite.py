c607028cd2601582b3b6d4c469d10111d6fd924f
chekulhan <46518731+chekulhan@users.noreply.github.com>
2024-03-14 04:30:32 +0100

    Update sqlite.py

--- sqlite.py
+++ sqlite.py
@@ -72,3 +72,75 @@ cur.execute("INSERT INTO empleados (id, name) VALUES (?, ?);", astuple(e))  # us
 
 conn.close()
 
+
+
+
+
+# Para crear una tabla con campo de autoincremento, usar: INTEGER PRIMARY KEY AUTOINCREMENT
+conn.execute("CREATE TABLE Users (id INTEGER PRIMARY KEY AUTOINCREMENT, nombre text, salario real)")
+
+
+# Actividad de clase CRUD para peliculas.
+import sqlite3
+
+print("Programa de BD CRUD")
+
+conn = sqlite3.connect('pelis.db')
+
+while True:
+
+    accion = int(input("Qué quieres hacer? 1. SELECT, 2. INSERT, 3. UPDATE 4. DELETE, 5. SELECT query 6. CLOSE"))
+
+    if accion == 1:
+        # mostrar todas las pelis
+        sSQL = "SELECT * FROM Peliculas"
+        cursor = conn.cursor()
+        cursor.execute(sSQL)
+        filas = cursor.fetchall()
+        for fila in filas:
+            print(fila)
+    elif accion == 2:
+        nombre = input("Cual es el nombre")
+        duracion = input("Cual es la duracion")
+        cur = conn.cursor()
+        sSQL = "INSERT INTO Peliculas (nombre, duracion) VALUES (?,?)"
+        cur.execute(sSQL, (nombre, duracion) )
+        conn.commit()
+        print("Tu transaccion ha funcionado correctamente")
+
+    elif accion == 3: # UPDATE
+        id = input("Qué numero quieres actualizar?")
+        duracion = input("Qué duracion tiene?")
+
+        cur = conn.cursor()
+        sSQL = "UPDATE Peliculas SET duracion = ? WHERE id = ?"
+        cur.execute(sSQL, (duracion, id) )
+        conn.commit()
+    
+    elif accion == 4: # DELETE
+        id = input("Qué numero quieres borrar?")
+
+        cur = conn.cursor()
+        sSQL = "DELETE FROM Peliculas WHERE id = " + id
+        cur.execute(sSQL )
+        conn.commit()
+            
+    elif accion == 5:
+        # mostrar todas las pelis
+        nombre = input("Qué peli buscas?")
+        nombre = nombre + "%"
+        sSQL = f"SELECT * FROM Peliculas WHERE nombre LIKE ?"
+        cursor = conn.cursor()
+        cursor.execute(sSQL, (nombre,))     # OJO, a veces hay que poner comma ","  - parece para indicar tuple
+        filas = cursor.fetchall()
+        for fila in filas:
+            print(fila)
+
+    elif accion==6:
+        print("cerrando")
+        conn.close()
+        break
+    else:
+        pass
+
+
