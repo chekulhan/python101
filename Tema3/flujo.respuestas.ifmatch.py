4a210943393438fd43ab6d9264c7acae7e9b5da3
chekulhan <46518731+chekulhan@users.noreply.github.com>
2024-10-15 17:41:56 +0200

    Update flujo.respuestas.ifmatch.py

--- Tema3/flujo.respuestas.ifmatch.py
+++ Tema3/flujo.respuestas.ifmatch.py
@@ -1,4 +1,47 @@
+# Programa de color
+color = input("¿Cúal es tu color favorito?")
 
+if color.lower() == "red":
+  print("Lo mio es rojo también")
+else:
+  print(f"Mi color favorito es rojo, no {color}")
+
+  
+# Zara productos
+qty = int(input("¿Cúal es la cantidad?"))
+
+if qty <0 or qty > 10:
+  print("Error: Introducir una cantidad >= 1 y <= 10")
+else:
+  print("Acceptado")
+  
+# Gastos de electricidad
+cantidad = 100000
+
+if cantidad > 1000:
+  print("Has gastado mucho este mes")
+elif cantidad > 500:
+  print("Has gastado bastante este mes")
+elif cantidad > 100:
+  print("Has gastado poco este mes")
+else:
+  print("No has gastado nada este mes")
+
+# Gastos de electricidad usando match
+gasto = float(input("Introduce el gasto de electricidad del cliente en euros: "))
+
+# Usar match para mostrar el mensaje correspondiente según el rango de gasto
+match gasto:
+    case _ if gasto >= 0 and gasto <= 99:
+        print("No has gastado nada este mes.")
+    case _ if gasto >= 100 and gasto <= 499:
+        print("Has gastado poco este mes.")
+    case _ if gasto >= 500 and gasto <= 999:
+        print("Has gastado bastante este mes.")
+    case _ if gasto >= 1000:
+        print("Has gastado mucho este mes.")
+    case _:
+        print("El valor ingresado no es válido.")
 
 
 # ----------------------------------------
