1813d65dd80a05afc9b98079b3edad0b2d27a9fd
chekulhan <46518731+chekulhan@users.noreply.github.com>
2024-11-11 11:26:45 +0100

    Update lists.py

--- Tema 2 - Tipos de datos/lists.py
+++ Tema 2 - Tipos de datos/lists.py
@@ -18,3 +18,29 @@ for n in notas:
 print(total)
 print(len(notas))
 print("media" + str(total/len(notas)))
+
+
+# vocales
+letras = ["a", "b", "e", "f", "o", "x", "c"]
+
+for l in letras:
+    if l in ["a", "e", "i", "o", "u"]:
+        print(True)
+    else: 
+        print(False)
+
+# camping
+maria = ["tienda", "linterna", "saco de dormir"]
+ana = ["linterna", "brújula", "cuchillo"]   
+carlos =  ["saco de dormir", "mochila", "cantimplora"] 
+
+equipamiento = maria + ana + carlos
+# Mostrar el equipamiento de cada persona
+print("Equipamiento de cada persona:")
+print(f"Maria lleva {maria}")
+print(f"Ana lleva {', '.join(ana)}")  # ojo!
+print(f"Carlos lleva {carlos}")
+
+# Mostrar la lista única de todos los objetos
+print("Lista de todos los objetos:")
+print(", ".join(equipamiento))
