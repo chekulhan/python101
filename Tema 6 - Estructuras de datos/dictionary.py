fc96096049c527af601de8bb693b53cc4c49e5b9
chekulhan <46518731+chekulhan@users.noreply.github.com>
2024-11-21 09:59:47 +0100

    gestor de coonfiguración

--- Tema 6 - Estructuras de datos/dictionary.py
+++ Tema 6 - Estructuras de datos/dictionary.py
@@ -66,3 +66,42 @@ for k, v in acciones.items():
     for precios in v:
         print(precios)
 
+
+
+# Termninar este programa para la gestión de la configuración de un servidor
+
+config = {
+    "server": "localhost",
+    "port": 8080,
+    "debug": True,
+    "log_level": "INFO"
+}
+
+def gestion_configuracion():
+    print("Elija una opción:")
+    print("1. Obtener configuración")
+    print("2. Actualizar configuración")
+    print("3. Listar todas las configuraciones")
+    
+    opcion = int(input("Introducur el número de la opción: "))
+
+    match opcion:
+        case 1:
+            clave = input("Introducur la clave que desea obtener: ")
+            # mostrar la clave deseada
+            # .....
+            
+        case 2:
+            clave = input("Ingrese la clave que desea actualizar: ")
+            valor = input("Ingrese el nuevo valor: ")
+            # Actualizar el valor de uno de las opciones de configuración
+            # ...
+        case 3:
+            # imprimir la configuración
+            # ....
+        case _:
+            print("Opción no válida. Intente nuevamente.")
+
+# Llamar a la función de gestión
+gestion_configuracion()
+
