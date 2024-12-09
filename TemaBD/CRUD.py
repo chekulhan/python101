e9f4bf153856cffd689116c2dfeef22a1e7b6e3b
chekulhan <46518731+chekulhan@users.noreply.github.com>
2024-12-09 09:48:10 +0100

    Create CRUD.py

---
+++ TemaBD/CRUD.py
@@ -0,0 +1,40 @@
+import sqlite3
+
+class Demo:
+    def __init__(self, ID, Name, Hint):
+        self.ID = ID
+        self.Name = Name
+        self.Hint = Hint
+        
+
+connection = sqlite3.connect("sqlite.db")
+
+cursor = connection.cursor()
+
+# comando INSERT con tuple
+
+cursor.execute("INSERT INTO demo (ID, Name, Hint) VALUES (?, ?, ?)", (201, 'My name', 'My hint'))
+connection.commit()
+
+# con un objeto
+demo = Demo(202, "nuevo nombre", "nueva pista")
+cursor.execute("INSERT INTO demo (ID, Name, Hint) VALUES (?, ?, ?)", (demo.ID, demo.Name, demo.Hint))
+
+# Para ver los resultados
+sSQL = "SELECT * FROM demo WHERE ID IN (200, 201, 202)"
+cursor.execute(sSQL)
+
+rows = cursor.fetchall()
+
+for row in rows:
+    print(row[0], row[1], row[2])
+
+
+connection.close()
+
+
+# UPDATE
+cursor.execute('UPDATE users SET age = ? WHERE name = ?', (35, 'Alice'))
+# DELETE
+# A single-value tuple requires a trailing comma (value,). Por ejemplo: cursor.execute('DELETE FROM users WHERE name = ?', ('Alice',))
+cursor.execute('DELETE FROM users WHERE name = ?', ('Alice',))
