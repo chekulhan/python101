4aecc5a0b9cf746fbad7b5b3fd6a4cc0d9d6802d
chekulhan <46518731+chekulhan@users.noreply.github.com>
2024-12-11 08:09:17 +0100

    ejercicio avanzado

--- Tema Avanzado/map.py
+++ Tema Avanzado/map.py
@@ -22,8 +22,12 @@ print(list(map(sum, numero1, numero2)))
 # ejercicio 1: sumar 2 a cada elemento de la lista
 # ejercicio 2:import math para aprovechar de la función math.sqrt
 # ejercicio 3:teniendo 3 listas de números, multiplicar cada numero en la lista
+# ejercicio 4: Calcular el precio total, para cada uno de los productos:
+quantities = [2, 3, 1]  # unidades compradas
+prices = [10.4, 8.3, 7.9]  # Precio por unidad
+discounts = [0.1, 0.2, 0.0]  # Discount as a fraction (10%, 20%, etc.)
 
-# respuesta 1
+# respuesta 1 ---------------------------------------
 numeros = [4, 6, 8, 2, 1, 5 ]
 
 lambda x: x + 2
@@ -44,3 +48,11 @@ numeros2 = [3, 6, 8, 9, 1, 5 ]
 numeros3 = [1, 5, 6, 7, 2, 6 ]
 
 print(list(map(lambda x, y, z: x*y*z, numeros1, numeros2, numeros3)))
+
+
+# respuesta 4
+quantities = [2, 3, 1]  # unidades compradas
+prices = [10.4, 8.3, 7.9]  # Precio por unidad
+discounts = [0.1, 0.2, 0.0]  # Discount as a fraction (10%, 20%, etc.)
+
+list(map(lambda q, p, d: q*p*(1-d), quantities, prices, discounts))
