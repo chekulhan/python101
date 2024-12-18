dae7a603804831262983279a816ad986cf1cedbc
chekulhan <46518731+chekulhan@users.noreply.github.com>
2024-12-18 08:20:33 +0100

    Update repaso1.py

--- Tema Repaso/repaso1.py
+++ Tema Repaso/repaso1.py
@@ -68,6 +68,15 @@ print(nombre)
 print(edad)         
 print(rol)  
 
+# PREGUNTA 5 desempaquetar
+numeros = [1, 2, 3, 4, 5]
+
+# resultado:
+print(primero)   # Output: 1
+print(mitad)  # Output: [2, 3, 4]
+print(ultimo)    # Output: 5
+
+
 # PREGUNTA 6 list comprehension
 # Usando list comprehension...
 nombres = ["Ana", "Luis", "Carlos", "Maria", "Pedro"]
