bfb01b4eb47ffc78a83d54d4b8ef6f27bac6ff36
chekulhan <46518731+chekulhan@users.noreply.github.com>
2024-12-18 08:14:08 +0100

    Update repaso.respuestas.py

--- Tema Repaso/repaso.respuestas.py
+++ Tema Repaso/repaso.respuestas.py
@@ -79,3 +79,11 @@ nombre, edad, rol = persona
 print(nombre)        
 print(edad)         
 print(rol)  
+
+# PREGUNTA 5 desempaquetar
+numeros = [1, 2, 3, 4, 5]
+
+# resultado:
+print(primero)   # Output: 1
+print(mitad)  # Output: [2, 3, 4]
+print(ultimo)    # Output: 5
