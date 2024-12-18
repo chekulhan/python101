5649856e2491c5100fd48f28176a674e78e05df1
chekulhan <46518731+chekulhan@users.noreply.github.com>
2024-12-18 08:26:01 +0100

    Create pandas.py

---
+++ Tema IA e Analisis Datos/pandas.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+datos = {
+    'Nombre': ['Juan', 'Isabel', 'José', 'David'],
+    'Age': [25, 30, 35, 40],
+    'City': ['Nueva York', 'Los Angeles', 'San Sebastián', 'Madrid']
+}
+
+df = pd.DataFrame(datos)
