3495c19767c667d37bba4c0f7efa5fab499fc9f6
chekulhan <46518731+chekulhan@users.noreply.github.com>
2025-09-07 11:13:25 +0200

    basic modules

--- Tema 1 - Input y Output/version.py
+++ Tema 1 - Input y Output/version.py
@@ -13,10 +13,48 @@ help(sys.version_info)
 help(sys.exit)
 
 
-# Actividad: Usando el módulo platform, crear un programa para mostrar información del plataform
+# Actividades
+# Actividad platform: Usando el módulo platform, crear un programa para mostrar información del plataform
+
 import platform
 
 
-# Respuesta:
+# Actividad datetime: ¿Cuantos segundos hay en un dia (demostrarlo)? ¿Qué es el epoch?
+# Si importamos el datetime asi, from datetime import datetime, ¿Qué cambios tenemos que realizar para que funcione?
+
+import datetime  
+
+epoch = datetime.datetime(1970, 1, 1)
+print(epoch)
+print(epoch.timestamp())
+
+# Mostrar el timestamp para el aterrizaje de la luna en 1969.
+
+
+# Actividad time
+import time
+
+print(time.time())
+print(time.ctime())
+
+# Convertir el formato de la hora en 2025-05-21, y luego añadir la hora, minutos y segundos
+print(time.strftime("%d-%m-%Y"))
+
+# El datetime module es más potente para trabajar con fechas
+from datetime import datetime
+
+# Datetime object for now
+now = datetime.now()
+print("datetime.now():", now)
+
+# Custom formatting
+print("strftime:", now.strftime("%Y-%m-%d %H:%M:%S"))
+
+--------
+
+# Respuestas:
 import platform
 print(platform.release(), platform.version(), platform.architecture())
+
+moon_landing = datetime(1969, 7, 20, 20, 17, 0)  # Apollo 11 moon landing
+
